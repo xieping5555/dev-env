1eac1a397e7582733f126c9bc0b98cc9e4002cfb
xieping.ekko <xieping.ekko@bytedance.com>
2024-03-31 13:43:12 +0800

    fix:setup

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,12 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 
 which sudo >/dev/null 2>&1
 if [ $? != 0 ]; then
     apt update
     apt install -y sudo
 fi
-
-set -ex
 
 # replace hosts for github domains
 sudo sh -c 'sed -i "/# GitHub520 Host Start/Q" /etc/hosts && curl https://raw.hellogithub.com/hosts >> /etc/hosts'
@@ -23,6 +21,7 @@ fi
 sudo apt install -y zsh && chsh -s /usr/bin/zsh $USER
 git clone https://github.com/ohmyzsh/ohmyzsh.git $ZSH_DIR
 sudo cp .zshrc $HOME
+zsh
 
 # install homebrew
 sudo apt-get install build-essential
